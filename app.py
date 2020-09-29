2c8a935639944b522da167d051b228fc57f251f2
boschman <benjamin.oschmann1@gmail.com>
2020-09-29 12:24:59 +0200

    Update name structure

---
+++ app.py
@@ -0,0 +1,21 @@
+import requests
+import pandas as pd
+import config
+
+df = pd.read_csv(r'C:\Users\boschman\PycharmProjects\pythonProject\testimport.csv')
+headers = {
+    'Authorization': 'Bearer '+config.TOKEN,
+    'Content-Type': 'application/json',
+}
+dflen = len(df)
+
+for i in range(len(df)):
+    payload = '{"long_url":"' + df.iloc[i][0] + '"}'
+    #print(payload)
+    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=payload)
+    #print(response)
+    jsonResponse = response.json()
+    print (jsonResponse["long_url"] , jsonResponse["id"])
+    file = open("resp_text.txt", "a+")
+    file.write(jsonResponse["long_url"]+" = \n"+jsonResponse["id"]+"\n")
+    file.close()
