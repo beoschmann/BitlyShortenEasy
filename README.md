02e291ba34d9f893e230afb519e2f4d656fab1ee
boschman <benjamin.oschmann1@gmail.com>
2020-09-29 12:55:37 +0200

    v1.0

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # BitlyShortenEasy
+Tool created to easily transform multiple Link into Bitly.
+
+App.py
+You need to insert your token to make it work.
+Token can be generated in your bitly account
+
+importurl.csv
+List of url to be shortened. Must include "https://"
+
+res_text.txt
+List of url shortened format : 
+Line 1 : LONG URL = 
+Line 2 : SHORT URL
+
+Thanks for using the tool.
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 import config
 
-df = pd.read_csv(r'C:\Users\boschman\PycharmProjects\pythonProject\testimport.csv')
+df = pd.read_csv(r'importurl.csv')
 headers = {
     'Authorization': 'Bearer '+config.TOKEN,
     'Content-Type': 'application/json',
@@ -15,7 +15,7 @@ for i in range(len(df)):
     response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=payload)
     #print(response)
     jsonResponse = response.json()
-    print (jsonResponse["long_url"] , jsonResponse["id"])
+    print (jsonResponse["long_url"]+" = \n"+jsonResponse["id"]+"\n")
     file = open("resp_text.txt", "a+")
     file.write(jsonResponse["long_url"]+" = \n"+jsonResponse["id"]+"\n")
     file.close()
