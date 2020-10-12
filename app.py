3156e67969b3ab4c22db094c13214d12a952f25e
boschman <benjamin.oschmann1@gmail.com>
2020-10-12 17:48:35 +0200

    v2.0.1
    Now built for the media newsletter format.
    Extract url from a Google Doc then Shorten them.

--- app.py
+++ app.py
@@ -7,15 +7,27 @@ headers = {
     'Authorization': 'Bearer '+config.TOKEN,
     'Content-Type': 'application/json',
 }
-dflen = len(df)
 
-for i in range(len(df)):
-    payload = '{"long_url":"' + df.iloc[i][0] + '"}'
-    #print(payload)
-    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=payload)
-    #print(response)
-    jsonResponse = response.json()
-    print (jsonResponse["long_url"]+" = \n"+jsonResponse["id"]+"\n")
-    file = open("resp_text.txt", "a+")
-    file.write(jsonResponse["long_url"]+" = \n"+jsonResponse["id"]+"\n")
-    file.close()
+#def shorten(fileimport) :
+def shorten(fileimport) :
+#    df = pd.read_csv(r'importurl.csv').
+#    df = pd.read_csv(r'fileimport')
+    headers = {
+        'Authorization': 'Bearer '+config.TOKEN,
+        'Content-Type': 'application/json',
+    }
+    for i in range(len(fileimport)):
+        payload = '{"long_url":"' + fileimport.iloc[i][0] + '"}'
+        #print(payload)
+        response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=payload)
+        #print(response)
+        jsonResponse = response.json()
+        print(jsonResponse)
+        print (jsonResponse["long_url"]+" = \n"+jsonResponse["id"]+"\n")
+        file = open("resp_text.txt", "a+")
+        file.write(jsonResponse["long_url"]+" = \n"+jsonResponse["id"]+"\n")
+        file.close()
+
+#de = pd.read_csv(r'urltoshorten.csv')
+#print(de)
+#shorten(de)
---
+++ extractjson.py
@@ -0,0 +1,92 @@
+from __future__ import print_function
+import json
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
+import app
+import pandas as pd
+import config
+
+
+headers = {
+    'Authorization': 'Bearer '+config.TOKEN,
+    'Content-Type': 'application/json',
+}
+######################################################################
+# IMPORT OF THE DOC
+######################################################################
+
+# Set doc ID, as found at `https://docs.google.com/document/d/YOUR_DOC_ID/edit`
+DOCUMENT_ID = '1I2Rn08OSfqulqpiK_3fyqnsVJvLcDwRgLK3dEth4y0k'
+#DOCUMENT_ID = '1L0V4-AoWy5UzTr4er36fB5VTo6MTywLZHMIi-28Lkno'
+
+# Set the scopes and discovery info
+SCOPES = 'https://www.googleapis.com/auth/documents.readonly'
+DISCOVERY_DOC = ('https://docs.googleapis.com/$discovery/rest?'
+                 'version=v1')
+
+# Initialize credentials and instantiate Docs API service
+store = file.Storage('token.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = discovery.build('docs', 'v1', http=creds.authorize(
+    Http()), discoveryServiceUrl=DISCOVERY_DOC)
+
+# Do a document "get" request and print the results as formatted JSON
+result = service.documents().get(documentId=DOCUMENT_ID).execute()
+file = open("extract_text.json", "w")
+file.write(json.dumps(result, indent=4, sort_keys=True))
+file.close()
+
+file = open("extract_text.json")
+docpars=json.load(file)
+
+#RESET OF URL input file
+file_out= open("urltoshorten.csv", "w")
+file_out.write("url\n")
+file_out.close()
+file_out= open("urltoshorten.csv", "a+")
+
+######################################################################
+# PARSING OF THE DOC and GET URL
+######################################################################
+
+k=-1
+docsparslen=len(docpars["body"]["content"])-1
+for i in range(0, len(docpars["body"]["content"])):
+    if k == docsparslen :
+        break
+    k=k+1
+    #   if docpars["body"]["content"][k]["paragraph"]["elements"][2]["textRun"]["textStyle"]["link"]["url"] != NULL :
+    if "paragraph" not in docpars["body"]["content"][k] :
+            k = k + 1
+    if docpars["body"]["content"][k]["paragraph"] != None :
+        if docpars["body"]["content"][k]["paragraph"]["elements"] != None :
+            while len(docpars["body"]["content"][k]["paragraph"]["elements"])!=4 :
+                if k == docsparslen :
+                    break
+                k = k +1
+            if k == docsparslen :
+                break
+            if docpars["body"]["content"][k]["paragraph"]["elements"][2]["textRun"] != None:
+                if docpars["body"]["content"][k]["paragraph"]["elements"][2]["textRun"]["textStyle"] != None :
+                    if docpars["body"]["content"][k]["paragraph"]["elements"][2]["textRun"]["textStyle"]["link"] != None :
+                        file_out.write(docpars["body"]["content"][k]["paragraph"]["elements"][2]["textRun"]["textStyle"]["link"]["url"]+"\n")
+                        #print(docpars["body"]["content"][k]["paragraph"]["elements"][2]["textRun"]["textStyle"]["link"]["url"])
+
+file_out.close()
+
+######################################################################
+# SHORTEN OF URL with BITLY API
+######################################################################
+
+de = pd.read_csv(r'urltoshorten.csv')
+app.shorten(de)
+#print(app.shorten(dx))
+
+
+
